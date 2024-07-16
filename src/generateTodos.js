4f4b6570591c2e8c0b84106eb5a65a16cf6505d6
MevWebDev <szymongrysiewicz@gmail.com>
2024-07-16 21:30:35 +0200

    Displaying descripiton only when it is here

--- src/generateTodos.js
+++ src/generateTodos.js
@@ -24,17 +24,19 @@ export function generateTodos() {
 
     const todoTitle = document.createElement("h3");
     todoTitle.textContent = todo.title;
+    todoDiv.appendChild(todoTitle);
 
-    const todoDesc = document.createElement("p");
-    todoDesc.textContent = todo.description;
+    if (todo.description) {
+      const todoDesc = document.createElement("p");
+      todoDesc.textContent = todo.description;
+      todoDiv.appendChild(todoDesc);
+    }
 
     const todoDate = document.createElement("p");
     todoDate.textContent = todo.dueDate;
 
     todoDiv.style.border = `3px solid ${borderColor}`;
 
-    todoDiv.appendChild(todoTitle);
-    todoDiv.appendChild(todoDesc);
     todoDiv.appendChild(todoDate);
     todoList.appendChild(todoDiv);
   });
